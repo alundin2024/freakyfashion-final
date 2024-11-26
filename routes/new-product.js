e03d2d8f390dcc3e976be693963c8189c9855faf
Anton Lundin <lundin09@gmail.com>
2024-11-26 13:26:36 +0100

    Tagit bort kommentarer, new-product.js bör tas bort.

--- routes/new-product.js
+++ routes/new-product.js
@@ -1,6 +1,4 @@
-// Gör två st API-anrop, en put, och en GET.
-// Skapa en tom lista (ex. products).
-// Min put ska ta emot data & addera det till den tomma listan i form av ett objekt i listan.
+// Denna fil bör kunnas tas bort?
 
 var express = require('express');
 var router = express.Router();
